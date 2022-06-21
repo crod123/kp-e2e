c4ec0a72ae13d86e1233ff08f806240d769c7297
Chris Rodriguez <christopher.rodriguez@cognizant.com>
2022-06-21 15:46:21 -0700

    initial

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "tests/cypress/support/e2e.{js,jsx,ts,tsx}"
+  },
+  downloadsFolder: "tests/cypress/downloads",
+  fixturesFolder: "tests/cypress/e2e",
+  supportFolder: "tests/cypress/support",
+  screenshotsFolder: "tests/cypress/screenshots",
+  videosFolder: "tests/cypress/videos",
+  env: {
+    "host": "https://healthy.kaiserpermanente.org/vaccinemonitor/consent"
+  },
+});
